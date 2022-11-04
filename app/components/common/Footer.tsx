0cc69a884b79663385cfb2951e99b04526585892
Wachimingo <alexanderguillen.adoc@gmail.com>
2022-11-04 13:29:08 -0600

    fix footer links

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -1,24 +1,24 @@
 import { FaFacebookF, FaGithubAlt, FaLinkedinIn, FaRegEnvelope } from "react-icons/fa";
 
 export const Footer = () => {
-    return (
-        <footer className="footer-wrapper">
-            <div className="footer-container">
-                <section className="social-media">
-                    <a href="https://www.facebook.com/halex007/" target="_blank" rel='noreferrer' role="button">
-                        <FaFacebookF />
-                    </a>
-                    <a href="https://www.linkedin.com/in/joshua-herrera-755143a7/" target="_blank" rel='noreferrer' role="button">
-                        <FaLinkedinIn />
-                    </a>
-                    <a href="https://github.com/Wachimingo" target="_blank" rel='noreferrer' role="button">
-                        <FaGithubAlt />
-                    </a>
-                </section>
-                <a href="mailto: joshua.herrera2@outlook.com">
-                    <FaRegEnvelope /> joshua.herrera2@outlook.com
-                </a>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className='footer-wrapper'>
+      <div className='footer-container'>
+        <section className='footer-links'>
+          <a href='https://www.facebook.com/halex007/' target='_blank' rel='noreferrer' role='button'>
+            <FaFacebookF />
+          </a>
+          <a href='https://www.linkedin.com/in/joshua-herrera-755143a7/' target='_blank' rel='noreferrer' role='button'>
+            <FaLinkedinIn />
+          </a>
+          <a href='https://github.com/Wachimingo' target='_blank' rel='noreferrer' role='button'>
+            <FaGithubAlt />
+          </a>
+        </section>
+        <a href='mailto: joshua.herrera2@outlook.com'>
+          <FaRegEnvelope /> joshua.herrera2@outlook.com
+        </a>
+      </div>
+    </footer>
+  );
+};
